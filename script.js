f2655fe87352d8352ee1ffedb05318fde909494d
Aaron Hunter <aaronphunter@icloud.com>
2022-01-02 11:43:24 -0700

    edit

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const hamburger = document.querySelector("nav img");
-const nav = document.querySelector("nav");
+window.onload = () => {
+    // Get document elements
+    const nav = document.querySelector("nav");
+    const hamburger = document.querySelector(".hamburger");
+    const ul =  document.querySelector("nav ul");
+    const a = document.querySelector("nav a");
 
-let navtoggle = false;
-hamburger.addEventListener("click", () => {
-    if (navtoggle == false) {
-        navtoggle = true
-        nav.className = "responsive";
-    } else {
-        navtoggle = false
-        nav.className = "";
+    // Set up variables
+    let linkHeight = parseInt(a.getBoundingClientRect().height) + "px";
+    let navExtendHeight = parseInt(a.getBoundingClientRect().height * 4) + "px";
+
+    // Set hamburger icon to the same height as the anchor links
+    hamburger.style.width = linkHeight;
+
+    // Listen for when the hamburger icon is clicked 
+    let navToggle = false;
+
+    function navOn() {
+        navToggle = true;
+        ul.style.height = navExtendHeight;
+        console.log("expanded nav");
     }
-    console.log(navtoggle)
-    
-});
+    function navOff() {
+        navToggle = false;
+        ul.style.height = "0px";
+        console.log("collapsed nav");
+    }
+    function toggleNav() {
+        if (navToggle === false) {
+            navOn();
+        } else {
+            navOff();
+        }
+    }
+    window.onresize = () => {
+        if (window.innerWidth > 500 && navToggle) {
+            navOff();
+            ul.style.height = "auto";
+        }
+    }
+    hamburger.addEventListener("click", toggleNav);
+}
+
 
