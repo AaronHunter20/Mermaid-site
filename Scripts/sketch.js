174195b0df07ebf904a0f1dd271da03cf75ceb4e
Aaron Hunter <aaronphunter@icloud.com>
2022-01-22 17:55:47 -0700

    stuff

--- Scripts/sketch.js
+++ Scripts/sketch.js
@@ -9,10 +9,10 @@ function draw() {
     interval += 1;
     rand = random(0, width + 1);
 
-     if (interval > 60) {
+    if (interval > 60) {
         interval = 0;
         bubbles.push(new Bubble(rand, height + 10));
-     }
+    }
 
     background(10, 39, 109);
     // stroke(175, 175, 255);
@@ -21,6 +21,7 @@ function draw() {
     for(let i = bubbles.length; i--;) {
         bubbles[i].move();
         bubbles[i].draw(bImg);
+        
         // if the mouse is within the range of a bubble, pop it. 
         if (dist(bubbles[i].x, bubbles[i].y, mouseX, mouseY) < bubbles[i].size/2) {
             bubbles[i].pop = true;
